494b1a16228117694a747d2879e3e075b56c0e9e
Kolen Cheung <christian.kolen@gmail.com>
2022-11-21 03:25:50 -0800

    remap number row; left & right most columns

--- moonlander_source/keymap.c
+++ moonlander_source/keymap.c
@@ -60,18 +60,18 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_DELETE,      KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       KC_EQUAL,       
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LEFT,                                        KC_UP,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,      
-    KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_RIGHT,                                                                       KC_DOWN,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
-    KC_LBRACKET,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RBRACKET,    
+    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_ESCAPE,                                      KC_DELETE,      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LEFT,                                        KC_UP,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,       
+    KC_LBRACKET,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_RIGHT,                                                                       KC_DOWN,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
+    KC_RBRACKET,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLASH,      
     KC_CAPSLOCK,    KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_LSHIFT,      OSL(1),                                                                                                         OSL(1),         KC_RSHIFT,      KC_RGUI,        KC_RALT,        KC_RCTRL,       KC_CAPSLOCK,    
     KC_SPACE,       KC_TAB,         TD(DANCE_0),                    TD(DANCE_1),    KC_BSPACE,      KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
-    KC_EQUAL,       KC_MINUS,       KC_0,           KC_9,           KC_8,           KC_7,           KC_DELETE,                                      KC_6,           KC_5,           KC_4,           KC_3,           KC_2,           KC_1,           KC_GRAVE,       
-    KC_BSLASH,      KC_P,           KC_O,           KC_I,           KC_U,           KC_Y,           KC_UP,                                          KC_LEFT,        KC_T,           KC_R,           KC_E,           KC_W,           KC_Q,           KC_TAB,         
-    KC_QUOTE,       KC_SCOLON,      KC_L,           KC_K,           KC_J,           KC_H,           KC_DOWN,                                                                        KC_RIGHT,       KC_G,           KC_F,           KC_D,           KC_S,           KC_A,           KC_ESCAPE,      
-    KC_RBRACKET,    KC_SLASH,       KC_DOT,         KC_COMMA,       KC_M,           KC_N,                                           KC_B,           KC_V,           KC_C,           KC_X,           KC_Z,           KC_LBRACKET,    
+    KC_MINUS,       KC_0,           KC_9,           KC_8,           KC_7,           KC_6,           KC_DELETE,                                      KC_ESCAPE,      KC_5,           KC_4,           KC_3,           KC_2,           KC_1,           KC_GRAVE,       
+    KC_EQUAL,       KC_P,           KC_O,           KC_I,           KC_U,           KC_Y,           KC_UP,                                          KC_LEFT,        KC_T,           KC_R,           KC_E,           KC_W,           KC_Q,           KC_TAB,         
+    KC_QUOTE,       KC_SCOLON,      KC_L,           KC_K,           KC_J,           KC_H,           KC_DOWN,                                                                        KC_RIGHT,       KC_G,           KC_F,           KC_D,           KC_S,           KC_A,           KC_LBRACKET,    
+    KC_BSLASH,      KC_SLASH,       KC_DOT,         KC_COMMA,       KC_M,           KC_N,                                           KC_B,           KC_V,           KC_C,           KC_X,           KC_Z,           KC_RBRACKET,    
     KC_TRANSPARENT, KC_RCTRL,       KC_RALT,        KC_RGUI,        KC_RSHIFT,      KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_LSHIFT,      KC_LGUI,        KC_LALT,        KC_LCTRL,       KC_TRANSPARENT, 
     KC_ENTER,       KC_BSPACE,      KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TAB,         KC_SPACE
   ),
